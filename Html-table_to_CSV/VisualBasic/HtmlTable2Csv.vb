f4e03d0e7880627647f6efc480766a62415c8113
Juan Pablo Enrici <jpenrici@gmail.com>
2023-04-22 18:05:33 -0300

    Add files via upload

---
+++ Html-table_to_CSV/VisualBasic/HtmlTable2Csv.vb
@@ -0,0 +1,325 @@
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+'
+' HTML Table to CSV
+'
+
+Friend Module HtmlTable2Csv
+
+    Public Const EOL As String = vbLf
+    Public Const TAB As String = vbTab
+    Public Const DELIM As String = ";"
+    Public Const SPACE As String = " "
+
+    Public Sub Main(args As String())
+
+        'HtmlTable2Csv
+        'Run(args)
+
+        'Test
+        'Test_HtmlTable2Csv.Test_ChackTag()
+        Test_HtmlTable2Csv.Test_Table2Csv()
+
+    End Sub
+
+    Private Sub Run(args As String())
+
+        Dim msg As String
+        msg = "Use: HtmlTable2Csv.exe if=inputfile of=outputfile.csv delim=delimiter"
+
+        Dim msgHelp As String
+        msgHelp = "For delimiters with special characters use quotation marks. " +
+                  "For example: delim='\\t' or delim=TAB"
+
+        If args.Length < 1 Then
+            Console.WriteLine(msg)
+            Exit Sub
+        End If
+
+        Dim fileIn As String = ""
+        Dim fileOut As String = ""
+        Dim delimiter As String = ""
+
+        For Each arg In args
+            If arg.StartsWith("if=") And fileIn = "" Then
+                fileIn = arg.Substring(3)
+            End If
+            If arg.StartsWith("of=") And fileOut = "" Then
+                fileOut = arg.Substring(3)
+            End If
+            If arg.StartsWith("delim=") And delimiter = "" Then
+                delimiter = arg.Substring(6)
+            End If
+            If arg.StartsWith("--help") Or arg.StartsWith("-h") Then
+                Console.WriteLine(msg)
+                Console.WriteLine(msgHelp)
+                Exit Sub
+            End If
+        Next
+
+        If fileIn = "" Then
+            Console.WriteLine(msg)
+            Exit Sub
+        End If
+
+        If fileOut = "" Then
+            fileOut = fileIn + ".csv"
+        End If
+
+        Console.WriteLine("Input : " + fileIn)
+        Console.WriteLine("Output: " + fileOut)
+
+        msg = "Delimiter: [" + delimiter + "] "
+        If delimiter = "" Then
+            delimiter = DELIM
+            msg += "(Empty! Use Default '" + delimiter + "')"
+        End If
+        If delimiter = "\\t" Or delimiter.ToLower = "tab" Then
+            delimiter = TAB
+            msg += "Tab"
+            Console.WriteLine(msg)
+        End If
+
+        Dim text As String = Table2Csv(fileIn, delimiter)
+        If Len(text) = 0 Then
+            Console.WriteLine("Invalid file or does not html table!")
+        Else
+            'Output
+            'Save(fileOut, text)
+        End If
+
+        Console.WriteLine("Finished.")
+
+    End Sub
+
+    Public Function CheckTag(ByVal line As String, Optional ByVal viewResult As Boolean = False) As Array
+        'Check string and get data if it's an <td> or <th> tag
+
+        'Standard return
+        '{column, currentRow, text}
+        Dim data() As String = {"0", "0", ""}
+
+        'Prepare
+        line = line.Replace(EOL, "")
+        line = RTrim(LTrim(line))
+
+        Dim result As Boolean
+        result = (Len(line) > 0) And (line.StartsWith("<td") Or line.StartsWith("<th")) And
+            (line.EndsWith("</td>") Or line.EndsWith("</th>"))
+
+        If result Then
+            'Find cell span
+            data = {"1", "0", ""}
+            Dim cellSpan() As String = {"colspan", "rowspan"}
+            For i = 0 To 1
+                Try
+                    Dim pattern = cellSpan(i) + "=\""[0-9]+"""
+                    Dim m As Match = Regex.Match(line, pattern, RegexOptions.IgnoreCase)
+                    If m.Success Then
+                        Dim substr As String = m.Value
+                        Dim num As String = Regex.Match(substr, "[0-9]+").Value
+                        data(i) = num
+                    End If
+                Catch
+                    Console.WriteLine("Regex Error!")
+                End Try
+            Next i
+            'Find value
+            data(2) = Regex.Replace(line, "<.?t[d,h].*?>", "")
+        End If
+
+        'Result
+        If viewResult Then
+            Console.WriteLine(line)
+            Console.WriteLine(String.Format("[{0}, {1}, {2}]", data(0), data(1), data(2)))
+        End If
+
+        CheckTag = data
+
+    End Function
+
+    Public Function StrTable2Csv(ByVal text As String, Optional ByVal delimiter As String = DELIM) As String
+        'input : string (Html), output : string (CSV)
+
+        Dim csv As String = ""
+
+        If Len(text) = 0 Then
+            Console.WriteLine("Empty text!")
+        Else
+            'Check <table> tag
+            If Not Regex.IsMatch(text, "<table.*?>", RegexOptions.IgnoreCase) Then
+                Console.WriteLine("Html table not found!")
+            Else
+                Console.WriteLine("Html Table found! Build CSV ... ")
+
+                'Prepare
+                text = text.Replace("></td>", "> </td>")
+                text = text.Replace("</table>", EOL + "</table>")
+                text = text.Replace("><", ">" + EOL + "<")
+
+                'Process    
+                Dim table As New ArrayList
+                Dim rows As Integer = 0
+                Dim maxCols As Integer = 0
+                Dim maxRows As Integer = 0
+                Dim flagTable As Boolean = False
+
+                Dim lines() As String = Split(text, EOL)
+                For i = 0 To lines.Length - 1
+                    'Prepare
+                    Dim line As String = lines(i).Replace(EOL, "")
+                    line = RTrim(LTrim(line))
+                    'Read
+                    If flagTable = False Then
+                        'Check <table> tag
+                        If line.StartsWith("<table") Then
+                            rows = 0
+                            table.Clear()
+                            flagTable = True
+                            Continue For
+                        End If
+                    End If
+                    If flagTable = True Then
+                        'Check <tr> tag
+                        If line.StartsWith("<tr") Then
+                            maxRows += 1
+                            Continue For
+                        End If
+                        If line.StartsWith("</tr>") Then
+                            rows += 1
+                            Continue For
+                        End If
+                        'Check <td> And <th> tag
+                        If line.StartsWith("<td") Or line.StartsWith("<th") Then
+                            Dim data As Array = CheckTag(line, True)
+                            maxCols += Convert.ToDecimal(data(0))
+                            maxRows += Convert.ToDecimal(data(1))
+                            Dim col() As String = {data(0), data(1), data(2), rows}
+                            table.Add(col)
+                            Continue For
+                        End If
+                        'Check other cases
+                        Dim state As Integer
+                        state = If(line.StartsWith("<table"), 1, 0)      'table reopened            
+                        state = If(line.StartsWith("</table"), 2, state) 'table closed 
+                        state = If(i >= lines.Length - 2, 3, state)      'end of text  
+                        'Table To CSV
+                        If state > 0 Then
+                            flagTable = (state = 1)
+                            'Rebuild table
+                            Dim matrix(maxRows + 1, maxCols + 1) As String
+                            For r = 0 To maxRows
+                                For c = 0 To maxCols
+                                    matrix(r, c) = ""
+                                Next c
+                            Next r
+                            Dim y As Integer = 0
+                            Dim x As Integer = 0
+                            Dim currentRow As Integer = 0
+                            For Each currentCol In table
+                                If currentRow <> currentCol(3) Then
+                                    currentRow = currentCol(3)
+                                    y += 1
+                                    x = 0
+                                End If
+                                Dim colspan As Integer = currentCol(0)
+                                Dim rowspan As Integer = currentCol(1)
+                                Dim value As String = currentCol(2)
+                                While Len(matrix(y, x)) > 0 And x < maxCols '!Empty
+                                    x += 1
+                                End While
+                                If colspan > 0 And rowspan = 0 Then         'only colspan
+                                    For dx = 0 To colspan - 1
+                                        matrix(y, x + dx) = SPACE
+                                    Next dx
+                                End If
+                                If colspan = 0 And rowspan > 0 Then         'only rowspan
+                                    For dy = 0 To rowspan - 1
+                                        matrix(y + dy, x) = SPACE
+                                    Next dy
+                                End If
+                                If colspan > 0 And rowspan > 0 Then         'colspan And rowspan
+                                    For dx = 0 To colspan - 1
+                                        For dy = 0 To rowspan - 1
+                                            matrix(y + dy, x + dx) = SPACE
+                                        Next dy
+                                    Next dx
+                                End If
+                                matrix(y, x) = value
+                                x += colspan
+                            Next currentCol
+                            'Convert CSV
+                            Dim txtTable As String = ""
+                            For r = 0 To maxRows
+                                Dim txtRow As String = ""
+                                For c = 0 To maxCols
+                                    txtRow += matrix(r, c) + delimiter
+                                Next c
+                                txtTable += txtRow.TrimEnd(delimiter) + EOL
+                            Next r
+                            csv += txtTable
+                            table.Clear()
+                        End If
+                    End If
+                Next i
+            End If
+        End If
+
+        Console.WriteLine(csv)
+
+        StrTable2Csv = csv
+
+    End Function
+
+    Public Function Table2Csv(ByVal path As String, ByVal delimiter As String) As String
+        'input : file text (Html), output : string (CSV)
+
+        Console.WriteLine("Load: " + path)
+        Dim text As String = Load(path)
+        Dim csv As String = ""
+
+        If Len(text) < 0 Then
+            Console.WriteLine("Empty file!")
+        Else
+            csv = StrTable2Csv(text, delimiter)
+        End If
+
+        Table2Csv = csv
+
+    End Function
+
+    Public Function Load(ByVal filename As String) As String
+
+        Dim text As String = ""
+
+        If Len(filename) > 0 Then
+            Try
+                Dim fileReader As String
+                fileReader = FileIO.FileSystem.ReadAllText(filename, Encoding.UTF8)
+                text = fileReader
+            Catch
+                Console.WriteLine("Error: " + filename)
+            End Try
+        End If
+
+        Load = text
+
+    End Function
+
+    Private Sub Save(ByVal filename As String, ByVal text As String)
+
+        If Len(filename) > 0 And Len(text) > 0 Then
+            Dim home As String = Environment.GetFolderPath(Environment.SpecialFolder.Personal)
+            Dim path As String = home + filename
+
+            Dim file As StreamWriter
+            file = FileIO.FileSystem.OpenTextFileWriter(path, True)
+            file.WriteLine(text)
+            file.Close()
+        End If
+
+    End Sub
+
+End Module
---
+++ Html-table_to_CSV/VisualBasic/Test_HtmlTable2Csv.vb
@@ -0,0 +1,164 @@
+﻿Module Test_HtmlTable2Csv
+
+    Sub Test_Table2Csv()
+
+        Dim html As String
+        Dim exp As String
+        Dim res As String
+
+        Dim home As String = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\html\"
+
+        ' 0 - non-existent table
+        html = Load(home + "test0.html")
+        exp = ""
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 1 - basic
+        html = Load(home + "test1.html")
+        exp = "A;B;C" + EOL + "1;2;3" + EOL + "4;5;6" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 2 - colspan
+        html = Load(home + "test2.html")
+        exp = "A; ;B" + EOL + "1;2;3" + EOL + "4;5;6" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 3 - rowspan
+        html = Load(home + "test3.html")
+        exp = "A;1" + EOL + "B;2" + EOL + " ;3" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 4 - colspan and rowspan
+        html = Load(home + "test4.html")
+        exp = "1;2; ; ; " + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 5 - single line
+        html = Load(home + "test5.html")
+        exp = "A;B; ; ; " + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 6 - two tables
+        html = Load(home + "test6.html")
+        exp = "A;B;C" + EOL + "1;2;3" + EOL + "4;5;6" + EOL + "D;E;F" + EOL + "7;8;9" + EOL + " ;10;11" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 7 - only table
+        html = Load(home + "test7.html")
+        exp = "A;B;C" + EOL + "1;2;3" + EOL + "4;5;6" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 8 - broken table
+        html = Load(home + "test8.html")
+        exp = "A;B;C" + EOL + "1;2;3" + EOL + "4;5" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 9 - broken tables
+        html = Load(home + "test9.html")
+        exp = "A;B;C" + EOL + "1;2;3" + EOL + "4;5" + EOL + "A;B;C" + EOL + "1;2;3" + EOL + "4;5" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 10 - extra
+        html = Load(home + "test10.html")
+        exp = "1;2; ; ; " + EOL + " ;3;4;5;6" + EOL + "7;8;9;10;11" + EOL + " ; ; ; ; ;12" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        ' 11 - <td></td>
+        html = Load(home + "test11.html")
+        exp = "AA;BB;CC" + EOL + " ;  ;1111" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        Console.WriteLine("Test delimiter ...")
+        html = Load(home + "test1.html")
+        exp = "A" + TAB + "B" + TAB + "C" + EOL +
+              "1" + TAB + "2" + TAB + "3" + EOL +
+              "4" + TAB + "5" + TAB + "6" + EOL
+        res = StrTable2Csv(html, TAB)
+        Debug.Assert(exp = res)
+
+        Console.WriteLine("Test string ...")
+        html = "<table><tr><th>AA</th><th>BB</th><th>CC</th></tr>" +
+               "<tr><td>11</td><td>22</td><td>33</td></tr>" +
+               "<tr><td>44</td><td>55</td><td>66</td></tr>" +
+               "</table>"
+        exp = "AA" + TAB + "BB" + TAB + "CC" + EOL +
+              "11" + TAB + "22" + TAB + "33" + EOL +
+              "44" + TAB + "55" + TAB + "66" + EOL
+        res = StrTable2Csv(html, TAB)
+        Debug.Assert(exp = res)
+
+        html = "<table><th>AA</th><td>BB</td><td>CC</td></table>"
+        exp = "AA" + TAB + "BB" + TAB + "CC" + EOL
+        res = StrTable2Csv(html, TAB)
+        Debug.Assert(exp = res)
+
+        Console.WriteLine("Test with incomplete tables ...")
+        html = "<table><th>AA</th><td>Error!</table>"
+        exp = "AA" + EOL
+        res = StrTable2Csv(html)
+        Debug.Assert(exp = res)
+
+        html = "<table><th>AAA</th><td>Error" + EOL + "<table>"
+        exp = "AAA" + EOL
+        res = StrTable2Csv(html, TAB)
+        Debug.Assert(exp = res)
+
+        Console.WriteLine("Everything is OK!")
+
+    End Sub
+
+    Sub Test_ChackTag()
+
+        ' Test <td> and <th> tag
+        Dim arr() As String = {"0", "0", ""}
+        Debug.Assert(IsArrayEqual(CheckTag("", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<>", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<t >", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<tr>", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<t d >", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("      <td>", True), arr))
+        Debug.Assert(IsArrayEqual(CheckTag("<td></td>", True), {"1", "0", ""}))
+        Debug.Assert(IsArrayEqual(CheckTag("<th>   </th>", True), {"1", "0", "   "}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td   >Text</td>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<th   >Text</th>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td   >Text</th>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<th   >Text</td>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td>" + EOL + "Text" + EOL + "</td>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td bgcolor=""#000000"">Text</td>", True), {"1", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td colspan=""5"">Text</td>", True), {"5", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<th colspan=""5"">Text</th>", True), {"5", "0", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td colspan=""5"" rowspan=""25"">Text</td>", True), {"5", "25", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<th colspan=""5"" rowspan=""25"">Text</th>", True), {"5", "25", "Text"}))
+        Debug.Assert(IsArrayEqual(CheckTag("<td bgcolor=""#FFFFFF"" rowspan=""2"">Text Text</td>", True), {"1", "2", "Text Text"}))
+        Console.WriteLine("CheckTag() is OK!")
+
+    End Sub
+
+    Private Function IsArrayEqual(arr1 As Array, arr2 As Array) As Boolean
+
+        Dim result As Boolean
+        result = (arr1.Length >= 3 And arr2.Length >= 3) And
+            (arr1(0) = arr2(0) And arr1(1) = arr2(1) And arr1(2) = arr2(2))
+        If Not result Then
+            Console.WriteLine(String.Format("Expected: [{0}, {1}, {2}]", arr2(0), arr2(1), arr2(2)))
+            Console.WriteLine(String.Format("Result  : [{0}, {1}, {2}]", arr1(0), arr1(1), arr1(2)))
+        End If
+
+        IsArrayEqual = result
+
+    End Function
+
+End Module
